f39409bea47e382e061bc009ac82284de5c17aa0
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-12-02 21:10:25 +0300

    feat(demo2): Optimised statement close order

--- demo2/src/main/java/com/pluralsight/Main.java
+++ demo2/src/main/java/com/pluralsight/Main.java
@@ -32,6 +32,8 @@ public class Main {
             System.out.println(city);
         }
 // 3. Close the connection
+        results.close();
+        pStatement.close();
         connection.close();
 
 
