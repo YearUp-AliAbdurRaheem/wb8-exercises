424cc80d5b2ed2d7fd6b82d9c2f3385993007d61
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-12-03 14:21:33 +0300

    refactor(NorthwindTraders): Format Main.java spacing

--- NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -6,16 +6,14 @@ public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         if (args.length != 2) {
             System.out.println(
-                    "Application needs two arguments to run: " +
-                            "java com.pluralsight.UsingDriverManager <username> <password>");
+                "Application needs two arguments to run: " +
+                "java com.pluralsight.UsingDriverManager <username> <password>");
             System.exit(1);
         }
 
         // get the username and password from the command line args
         String username = args[0];
         String password = args[1];
-
-
 
         // load the MySQL Driver
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -25,61 +23,57 @@ public class Main {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/northwind",
-                    username,
-                    password);
-
+                "jdbc:mysql://localhost:3306/northwind",
+                username,
+                password);
 
             // define your query
-                    String ProductIDs_Query = "SELECT ProductID from Products;";
-                    String ProductNames_Query = "SELECT ProductName from Products;";
-                    String UnitPrices_Query = "SELECT UnitPrice from Products;";
-                    String UnitsInStock_Query = "SELECT UnitsInStock from Products;";
+            String ProductIDs_Query = "SELECT ProductID from Products;";
+            String ProductNames_Query = "SELECT ProductName from Products;";
+            String UnitPrices_Query = "SELECT UnitPrice from Products;";
+            String UnitsInStock_Query = "SELECT UnitsInStock from Products;";
 
             // Define statements
-                    PreparedStatement ProductIDs_Statement = connection.prepareStatement(ProductIDs_Query);
-                    PreparedStatement ProductNames_Statement = connection.prepareStatement(ProductNames_Query);
-                    PreparedStatement UnitPrices_Statement = connection.prepareStatement(UnitPrices_Query);
-                    PreparedStatement UnitsInStock_Statement = connection.prepareStatement(UnitsInStock_Query);
-
+            PreparedStatement ProductIDs_Statement = connection.prepareStatement(ProductIDs_Query);
+            PreparedStatement ProductNames_Statement = connection.prepareStatement(ProductNames_Query);
+            PreparedStatement UnitPrices_Statement = connection.prepareStatement(UnitPrices_Query);
+            PreparedStatement UnitsInStock_Statement = connection.prepareStatement(UnitsInStock_Query);
 
             // 2. Execute your query
-                    ResultSet ProductIDs = ProductIDs_Statement.executeQuery();
-                    ResultSet ProductNames = ProductNames_Statement.executeQuery();
-                    ResultSet UnitPrices = UnitPrices_Statement.executeQuery();
-                    ResultSet UnitsInStock = UnitsInStock_Statement.executeQuery();
+            ResultSet ProductIDs = ProductIDs_Statement.executeQuery();
+            ResultSet ProductNames = ProductNames_Statement.executeQuery();
+            ResultSet UnitPrices = UnitPrices_Statement.executeQuery();
+            ResultSet UnitsInStock = UnitsInStock_Statement.executeQuery();
+
             // process the results
                 System.out.printf("%-20s %-22s %-6s %-6s%n", "Id", "Name", "Price", "Stock");
                 System.out.println("-- --------------------------------------- ------- -----");
 
-                while (ProductIDs.next() && ProductNames.next() && UnitPrices.next() && UnitsInStock.next()) {
+            while (ProductIDs.next() && ProductNames.next() && UnitPrices.next() && UnitsInStock.next()) {
                 int id = ProductIDs.getInt("ProductID");
                 String name = ProductNames.getString("ProductName");
                 double price = UnitPrices.getDouble("UnitPrice");
                 int stock = UnitsInStock.getInt("UnitsInStock");
                 
                 System.out.printf("%-2d %-40s %-7.2f %-6d%n", id, name, price, stock);
-}
-
+            }
 
             // 3. Close the connection
-                    ProductIDs.close();
-                    ProductNames.close();
-                    UnitPrices.close();
-                    UnitsInStock.close();
+            ProductIDs.close();
+            ProductNames.close();
+            UnitPrices.close();
+            UnitsInStock.close();
 
-                    ProductIDs_Statement.close();
-                    ProductNames_Statement.close();
-                    UnitPrices_Statement.close();
-                    UnitsInStock_Statement.close();
+            ProductIDs_Statement.close();
+            ProductNames_Statement.close();
+            UnitPrices_Statement.close();
+            UnitsInStock_Statement.close();
 
-                    connection.close();
-
-        }
-        finally {
-        if (connection != null) {
             connection.close();
-        }
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
         }
     }
 }
