49db35a453e260c65feea8a46f11c341f0381c59
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-12-10 12:15:20 +0300

    docs: Update Comments

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,21 @@
 package com.pluralsight;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
     final static Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         logMeLikeYouDo("☕");
     }
-    private static void logMeLikeYouDo(String input){
-        if(logger.isDebugEnabled()){
+
+    // This is a demo to show how logging works and the different logging levels
+    private static void logMeLikeYouDo(String input) {
+        if (logger.isDebugEnabled()) {
             logger.debug("This is debug : " + input);
         }
-        if(logger.isInfoEnabled()){
+        if (logger.isInfoEnabled()) {
             logger.info("This is info : " + input);
         }
         logger.warn("This is warn : " + input);
